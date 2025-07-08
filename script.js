4e1cd8e4039e9856e75bff05aec82414a5055d98
Kartik B H <95575463+kartikbh56@users.noreply.github.com>
2025-07-08 16:54:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
 // Person class definition
-function Person(name, age) {
+class Person {
+  constructor(name, age) {
     this.name = name;
     this.age = age;
-}
+  }
 
-// greet method for Person
-Person.prototype.greet = function() {
+  greet() {
     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-};
+  }
+}
 
 // Employee class definition (inherits from Person)
-function Employee(name, age, jobTitle) {
-    // Call the parent constructor
-    Person.call(this, name, age);
+class Employee extends Person {
+  constructor(name, age, jobTitle) {
+    super(name, age); // Call the parent class constructor
     this.jobTitle = jobTitle;
-}
+  }
 
-// Set up inheritance
-Employee.prototype = Object.create(Person.prototype);
-Employee.prototype.constructor = Employee;
-
-// jobGreet method for Employee
-Employee.prototype.jobGreet = function() {
+  jobGreet() {
     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-};
+  }
+}
 
 // Do not change code below this line
 window.Person = Person;
